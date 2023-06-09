65035b15ed934afb02af7201167d965fa6c4a660
ton-na <ikeuchi.riku.is0@is.naist.jp>
2023-06-09 02:51:52 +0000

    experiment 1~20 separate

--- 2/expriment.py
+++ 2/expriment.py
@@ -1,30 +1,39 @@
-import sys
-import resource
-sys.setrecursionlimit(10 ** 9)
-print(sys.getrecursionlimit())
 # 入力
-S = "268668743807663281359259975924239968838915518781704591221216502144648297573337534154488714432458672010933572906012936272617155562367959865483138529186273117849466634478519408432309577699417"
+S = "1718110212314413155616720819911"
 
 # 数分解の結果を格納するリスト
 ans = []
+
+# メモ化用の辞書
+memo = {}
 
 # 深さ優先探索
 def dfs(i, s):
     # 全ての文字を使い切ったら結果を出力
     if i == len(S):
-        ans.append(s)
+        ans.append(tuple(s))
         return
-    # 1桁の数を追加
-    if S[i] not in ans:
-        dfs(i + 1, s + " " + S[i])
-    # 2桁の数を追加（可能なら）
-    if S[i:i+2] not in ans:
-        if i < len(S) - 1 and S[i] != "0":
-            dfs(i + 2, s + " " + S[i:i+2])
+    # メモ化チェック
+    if (i, tuple(s)) in memo:
+        ans.extend(memo[(i, tuple(s))])
+        return
+    # 1桁の数を追加（0でなければ）
+    if S[i] != "0":
+        # 重複チェック
+        if int(S[i]) not in s:
+            dfs(i + 1, s + [int(S[i])])
+    # 2桁の数を追加（可能かつ0で始まらなければ）
+    if i < len(S) - 1 and S[i] != "0" and int(S[i:i+2]) <= 20:
+        # 重複チェック
+        if int(S[i:i+2]) not in s:
+            dfs(i + 2, s + [int(S[i:i+2])])
+    # メモ化更新
+    memo[(i, tuple(s))] = list(ans)
 
 # 最初の呼び出し
-dfs(0, "")
+dfs(0, [])
 
 # 結果の表示
 for a in ans:
-    print(a.strip())
+    print(" ".join(map(str, a)))
+
