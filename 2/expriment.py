92d79c760f69b58f5651b1978ef1dc1e1b4f850f
ton-na <ikeuchi.riku.is0@is.naist.jp>
2023-06-14 05:08:44 +0000

    2,3 Kadai

--- 2/expriment.py
+++ 2/expriment.py
@@ -1,5 +1,28 @@
+# 深さ優先探索
+def dfs(i, s):
+    # 全ての文字を使い切ったら結果を出力
+    if i == len(S):
+        ans.append(tuple(s))
+        return ans # ここでansを返す
+    # メモ化チェック
+    if (i, tuple(s)) in memo:
+        ans.extend(memo[(i, tuple(s))])
+        return ans # ここでansを返す
+    # 1桁の数を追加（0でなければ）
+    if S[i] != "0":
+        # 重複チェック
+        if int(S[i]) not in s:
+            dfs(i + 1, s + [int(S[i])])
+    # 2桁の数を追加（可能かつ0で始まらなければ）
+    if i < len(S) - 1 and S[i] != "0":
+        # 重複チェック
+        if int(S[i:i+2]) not in s:
+            dfs(i + 2, s + [int(S[i:i+2])])
+    # メモ化更新
+    memo[(i, tuple(s))] = list(ans)
+
 # 入力
-S = "1718110212314413155616720819911"
+S = "773181522128748272298129793949024594941105042259227704876614744625899327316561366965719566235587963422604546861151544329262033575401768805730883919989518388467631485645315837491733787983136"
 
 # 数分解の結果を格納するリスト
 ans = []
@@ -7,33 +30,10 @@ ans = []
 # メモ化用の辞書
 memo = {}
 
-# 深さ優先探索
-def dfs(i, s):
-    # 全ての文字を使い切ったら結果を出力
-    if i == len(S):
-        ans.append(tuple(s))
-        return
-    # メモ化チェック
-    if (i, tuple(s)) in memo:
-        ans.extend(memo[(i, tuple(s))])
-        return
-    # 1桁の数を追加（0でなければ）
-    if S[i] != "0":
-        # 重複チェック
-        if int(S[i]) not in s:
-            dfs(i + 1, s + [int(S[i])])
-    # 2桁の数を追加（可能かつ0で始まらなければ）
-    if i < len(S) - 1 and S[i] != "0" and int(S[i:i+2]) <= 20:
-        # 重複チェック
-        if int(S[i:i+2]) not in s:
-            dfs(i + 2, s + [int(S[i:i+2])])
-    # メモ化更新
-    memo[(i, tuple(s))] = list(ans)
-
 # 最初の呼び出し
-dfs(0, [])
+result = dfs(0, []) # ここでresultに結果を代入
 
-# 結果の表示
-for a in ans:
-    print(" ".join(map(str, a)))
+# 結果の表示ではなく、リストとして扱う
+print(result[0]) # 最初の要素を表示
+print(result[-1]) # 最後の要素を表示
 
