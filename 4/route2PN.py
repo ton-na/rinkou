520bc5b14516b3b3d30a2cb94e176aac284eaebb
ton-na <ikeuchi.riku.is0@is.naist.jp>
2023-06-30 05:42:11 +0000

    comment

--- 4/route2PN.py
+++ 4/route2PN.py
@@ -2,10 +2,10 @@ import decimal
 
 decimal.getcontext().prec = 100
 
-def sqrt2():
+def sqrt2(): # √２の精度の高い表示
     return decimal.Decimal(2).sqrt()
 
-def is_prime(n):
+def is_prime(n): # 素数判定　エラトステネスのふるい
     if n <= 1:
         return False
     for i in range(2, int(n ** 0.5) + 1):
@@ -15,9 +15,9 @@ def is_prime(n):
 
 def find_prime():
     ans = []
-    sqrt2_str = str(sqrt2())[2:]
-    for i in range(len(sqrt2_str) - 10):
-        num = int(sqrt2_str[i:i+11])
+    sqrt2_str = str(sqrt2())[2:] # 1.414...から整数部分を除いた部分
+    for i in range(len(sqrt2_str) - 10): # 参照が後ろに行き過ぎるとバグるので、最後を手前に
+        num = int(sqrt2_str[i:i+11]) # n個から11個をとってくる
         if is_prime(num) & len(str(num)):
             ans.append(str(i))
             ans.append(str(num))
