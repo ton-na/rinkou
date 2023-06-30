554f4ead35dce483373c9b3478a0aad249a53334
ton-na <ikeuchi.riku.is0@is.naist.jp>
2023-06-30 03:48:11 +0000

    Finish?

--- 4/route2PN.py
+++ 4/route2PN.py
@@ -1,19 +1,25 @@
 import math
+from sympy import primerange
 
 def is_prime(num):
+    n = int(math.sqrt(num)) + 1
+    primlist = list(primerange(2,n))
+
     if num < 2:
         return False
-    for i in range(2, int(math.sqrt(num)) + 1):
+    for i in primlist:
         if num % i == 0:
             return False
     return True
 
 def decimal_to_prime(decimal):
-    integer = int(decimal * 10**5)
+    integer = int(decimal * 10**11)
     primes = []
-    for i in range(integer, integer + 10**5):
+    for i in range(integer, integer + 10**11):
         if is_prime(i):
             primes.append(i)
-    return primes
+            if len(str(i)) == 11:
+                return i
+    return None
 
 print(decimal_to_prime(math.sqrt(2) - int(math.sqrt(2))))
