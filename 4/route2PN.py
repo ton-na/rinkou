25701240804aa051b1f8c384840c544ce49deb16
ton-na <ikeuchi.riku.is0@is.naist.jp>
2023-06-30 06:23:43 +0000

    comment

--- 4/route2PN.py
+++ 4/route2PN.py
@@ -11,7 +11,7 @@ def find_prime():
     sqrt2_str = str(sqrt2())[2:] # 1.414...から整数部分を除いた部分
     for i in range(len(sqrt2_str) - 10): # 参照が後ろに行き過ぎるとバグるので、最後を手前に
         num = int(sqrt2_str[i:i+11]) # n個から11個をとってくる
-        if isprime(num) & len(str(num)): # 巨大数などの特殊な数字を除いた中で一番早い素数判定アルゴリズム,AKS素数判定法
+        if isprime(num) & len(str(num)): # メルセンヌ数などの特殊な数字を除いた中で一番早い素数判定アルゴリズム,AKS素数判定法
             ans.append(str(i))
             ans.append(str(num))
     return ans
