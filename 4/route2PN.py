d005f7e670655093b0ffb8d766811cedfc69ccad
ton-na <ikeuchi.riku.is0@is.naist.jp>
2023-07-06 02:36:33 +0000

    short ver1.1

--- 4/route2PN.py
+++ 4/route2PN.py
@@ -1,6 +1,5 @@
 import decimal
 from sympy.ntheory.primetest import isprime
-decimal.getcontext().prec = 1000
+decimal.getcontext().prec = 100
 sqrt2_str = str(decimal.Decimal(2).sqrt())[2:] # 1.414...から整数部分を除いた部分
-ans = [str(i) +"番目: "+ str(int(sqrt2_str[i:i+11])) for i in range(len(sqrt2_str) - 10) if isprime(int(sqrt2_str[i:i+11])) & len(str(int(sqrt2_str[i:i+11])))]
-print(ans)
+print([str(i) +"番目: "+ str(int(sqrt2_str[i:i+11])) for i in range(len(sqrt2_str) - 10) if isprime(int(sqrt2_str[i:i+11])) & len(str(int(sqrt2_str[i:i+11])))])
